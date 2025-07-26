fa546e5021dc7d4c496264f283179dc263056a80
Isac2006 <96455960+Isac2006@users.noreply.github.com>
2025-07-25 21:25:34 -0300

    Update dinheiro.js
    deixei mais organizado e mais preparado para futura atualizaçoes

--- dinheiro.js
+++ dinheiro.js
@@ -6,22 +6,30 @@ const form = document.getElementById("formulario")
   });
 let opçaoe = document.getElementById("opçao")
 let opcao = opçaoe.value
-let vUSA = 5.00
-let vEUR = 6.00
-let vLIB = 7.00
+let vUSA = 5.30
+let vEUR = 6.50
+let vLIB = 7.80
 
 let valorReais = parseFloat(document.getElementById("reais").value);
  switch (opcao) {
     case "Usa":
-      total = valorReais * vUSA;
+      convertervalor(valorReais,vUSA,"$")
       break;
     case "Eur":
-      total = valorReais * vEUR;
+      convertervalor(valorReais,vEUR,"E")
       break;
     case "Lib":
-      total = valorReais * vLIB;
+    convertervalor(valorReais,vLIB,"L")
       break;
   }
+  function convertervalor(amont,moeda, simbolo){
+
 let h4 = document.getElementById("resultado")
-h4.textContent = total
+let convertido = amont * moeda
+convertido = convertido.toFixed(2)
+ let total = convertido.toString().replace(".",",")
+let mensagem = `${simbolo} ${total}`
+h4.textContent = mensagem
+console.log("ola")
+  }
 }
